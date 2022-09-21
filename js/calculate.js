1f61f14533d16e774d054586255500f4965def7e
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-21 11:40:11 +0900

    単項演算

--- js/calculate.js
+++ js/calculate.js
@@ -1,7 +1,15 @@
 var hp = 3
-
+//2項演算
 console.log(hp + 1)
 console.log(hp - 2)
 console.log(hp * 3)
 console.log(hp / 2)
 console.log(hp % 2)
+
+// 単項演算
+hp = 3
+hp++
+console.log(hp)
+hp--
+console.log(hp)
+
