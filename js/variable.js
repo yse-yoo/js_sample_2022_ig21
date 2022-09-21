c5e59e6d19f0d26f0e187600e72a749dbd8f3880
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-21 11:13:16 +0900

    add type values

--- js/variable.js
+++ js/variable.js
@@ -3,3 +3,18 @@ var monster
 monster = 'スライム'
 // コンソール表示
 console.log(monster)
+
+var amount = 2
+var price = 1000
+var discount = -100
+
+console.log(amount, price, discount)
+
+var average = 0.333
+var speed = 45.5
+console.log(average, speed)
+
+var is_active = true
+console.log(is_active)
+is_active = false
+console.log(is_active)
