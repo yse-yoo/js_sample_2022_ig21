96de4b6ea38332cd24fefecf837359b3338a5ce5
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-28 10:59:39 +0900

    add function

---
+++ js/function.js
@@ -0,0 +1,15 @@
+
+/**
+ * 関数定義（メソッド）
+ * @param number x 
+ * @returns number
+ */
+function calculate(x) {
+    var y = x + 5
+    return y
+}
+
+// 関数（メソッド）の実行
+var answer = calculate(3)
+console.log(answer)
+
