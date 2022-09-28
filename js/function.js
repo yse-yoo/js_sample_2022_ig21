f1e0fc2793817b6d40bfce460c704dfd9d6dfaad
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-09-28 11:05:25 +0900

    add totalPrice

--- js/function.js
+++ js/function.js
@@ -13,3 +13,16 @@ function calculate(x) {
 var answer = calculate(3)
 console.log(answer)
 
+
+//税率
+const TAX = 0.1
+
+//メソッド定義
+function totalPrice(price, amount) {
+    var total_price = price * amount * (1 + TAX)
+    return total_price.toFixed()
+}
+
+//メソッド実行
+var total_price = totalPrice(300, 5)
+console.log(total_price)
