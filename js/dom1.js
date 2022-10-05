e6b256e0bfc87ce06d261938b810f9ca0062077b
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-05 11:25:00 +0900

    add innerHtml

--- js/dom1.js
+++ js/dom1.js
@@ -1,2 +1,9 @@
 var priceElement = document.getElementById('price')
 console.log(priceElement)
+
+var price = priceElement.textContent
+console.log(price)
+
+var messageElement = document.getElementById('message')
+messageElement.innerHTML = "<span>いらっしゃい</span>"
+// messageElement.textContent = "<span>いらっしゃい</span>"
