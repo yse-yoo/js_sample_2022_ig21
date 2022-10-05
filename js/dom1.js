505c5c62c3d4744cf6fc62d41f6d2f46ba80855b
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-05 11:50:15 +0900

    add dice

--- js/dom1.js
+++ js/dom1.js
@@ -6,4 +6,15 @@ console.log(price)
 
 var messageElement = document.getElementById('message')
 messageElement.innerHTML = "<span>いらっしゃい</span>"
+// messageElement.innerText = "<span>いらっしゃい</span>"
 // messageElement.textContent = "<span>いらっしゃい</span>"
+
+var titleElement = document.getElementById('title')
+titleElement.innerHTML = "DICE"
+
+document.getElementById("result").innerHTML = randomNumber(1, 6)
+
+function randomNumber(min, max) {
+    var number = Math.floor(Math.random() * (max + 1 - min)) + min;
+    return number;
+}
