771ca806d80e8671b8861e69f8302c44f5c69e8b
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-05 09:57:46 +0900

    add outputP

--- js/document.js
+++ js/document.js
@@ -2,3 +2,12 @@
 document.write('<h1>プロフィール</h1>')
 document.write('<h2>名前</h2>')
 document.write('<p>東京　太郎</p>')
+
+var outputP = function (message) {
+    var tag = "<p>" + message + "</p>"
+    document.write(tag)
+}
+
+outputP("旅行")
+outputP("ゲーム")
+outputP("スポーツ")
