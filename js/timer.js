600c19934b2ca178b3fe5ec0634a54ec183d9e88
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-12 11:26:41 +0900

    add setinterval

--- js/timer.js
+++ js/timer.js
@@ -1,8 +1,15 @@
+var counterElement = document.getElementById("counter")
+var interval = 1000
+var timer
 
-const start = () => { 
+const start = () => {
+    console.log("start")
+    var count = 0
+    timer = setInterval(function () {
 
+    }, interval)
 }
 
-const stop = () => { 
-
+const stop = () => {
+    console.log("stop")
 }
