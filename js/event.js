1cbf59e07e3a8de63ef846149a510146962d2cc2
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-12 10:35:06 +0900

    getElement

--- js/event.js
+++ js/event.js
@@ -1,9 +1,19 @@
 var characterElement = document.getElementById("character_name")
 var messageElement = document.getElementById("message")
+var itemNameElement = document.getElementById("item_name")
+var itemPriceElement = document.getElementById("item_price")
+var amountElement = document.getElementById("amount")
 
 // anfn
 const inputCharacterName = () => { 
     console.log("変更されました")
     var message = characterElement.value + "さん、いらっしゃい"
     messageElement.innerHTML = message
+}
+
+const buy = () => { 
+    var item_name = itemNameElement.innerText
+    var item_price = itemPriceElement.innerText
+    var amount = amountElement.innerText
+    console.log('buy')
 }
