ff55ae7ebccf2e040f2a3eae59196777db445936
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-12 12:07:30 +0900

    update start

--- js/timer.js
+++ js/timer.js
@@ -2,15 +2,21 @@ var counterElement = document.getElementById("counter")
 var interval = 1000
 var timer
 
+var count = 0
+var updateCount = function () {
+    count++
+    counterElement.innerHTML = count
+}
+
 const start = () => {
     console.log("start")
     clearInterval(timer)
+    timer = setInterval(updateCount, interval)
 
-    var count = 0
-    timer = setInterval(function () {
-        count++
-        counterElement.innerHTML = count
-    }, interval)
+    // timer = setInterval(function () {
+    //     count++
+    //     counterElement.innerHTML = count
+    // }, interval)
 }
 
 const stop = () => {
