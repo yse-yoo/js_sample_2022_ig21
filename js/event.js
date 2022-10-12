73840dba15425aa3a1887f69dc624415608cb0ff
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-12 09:56:28 +0900

    add message

--- js/event.js
+++ js/event.js
@@ -1,5 +1,9 @@
+var characterElement = document.getElementById("character_name")
+var messageElement = document.getElementById("message")
 
 // anfn
 const inputCharacterName = () => { 
     console.log("変更されました")
+    var message = characterElement.value + "さん、いらっしゃい"
+    messageElement.innerHTML = message
 }
