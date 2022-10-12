d675d1ea6ca49cc39433315e8a645e36004193ec
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-12 09:27:27 +0900

    add event file

--- js/dom1.js
+++ js/dom1.js
@@ -1,11 +1,13 @@
 var priceElement = document.getElementById('price')
 console.log(priceElement)
 
-var price = priceElement.textContent
+// var price = priceElement.textContent
+// var price = priceElement.innerText
+var price = priceElement.innerHTML
 console.log(price)
 
 var messageElement = document.getElementById('message')
-messageElement.innerHTML = "<span>いらっしゃい</span>"
+messageElement.innerHTML = '<span style="color: red;">いらっしゃい</span>'
 // messageElement.innerText = "<span>いらっしゃい</span>"
 // messageElement.textContent = "<span>いらっしゃい</span>"
 
