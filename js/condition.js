b06044946527124b2cd670bfa8ae1c842d797360
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-19 11:23:42 +0900

    add switch

--- js/condition.js
+++ js/condition.js
@@ -1,4 +1,3 @@
-
 var hp = 10
 var attack = 10
 var message = ""
@@ -11,3 +10,21 @@ if (hp > 0) {
     message = "モンスターを倒した"
 }
 document.getElementById("message").innerHTML = message
+
+var weekday = "水"
+var type = ""
+
+switch (weekday) {
+    case "水":
+        type = "燃えるゴミ" 
+        break;
+
+    case "金":
+        type = "燃えないゴミ" 
+        break;
+
+    default:
+        type = "回収なし" 
+        break;
+}
+document.getElementById("message").innerHTML = type
