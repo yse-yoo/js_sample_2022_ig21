292ee068b8da970e562132ecca328ece6e7f0f3b
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-19 10:09:46 +0900

    add timer

--- js/character.js
+++ js/character.js
@@ -3,10 +3,22 @@ character.style.position = 'absolute'
 character.style.left = 0
 character.style.top = '100px'
 
+var timer
+const interval = 100
+const step = 1
+
 const start = () => { 
     console.log('Start')
+
+    var count = 0
+    clearInterval(timer)
+    timer = setInterval(function() {
+        count++
+        console.log(count)
+    }, interval)
 }
 
 const stop = () => { 
     console.log('Stop')
+    clearInterval(timer)
 }
