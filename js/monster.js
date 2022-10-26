bc36345011519708e86374bb61fab6c9b0436e49
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-26 10:53:34 +0900

    add element

--- js/monster.js
+++ js/monster.js
@@ -5,4 +5,9 @@ var stage = document.getElementById('stage')
 var monsterElement1 = document.createElement('p')
 monsterElement1.innerHTML = 'スライム'
 
-// pタグを親要素に追加
+// pタグを親要素(stage)に追加
+stage.appendChild(monsterElement1)
+
+var monsterElement2 = document.createElement('p')
+monsterElement2.innerHTML = 'キメラ'
+stage.appendChild(monsterElement2)
