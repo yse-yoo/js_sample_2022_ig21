45d579aa2517ed0166de823e8e1118704c4d6ac5
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-26 09:33:01 +0900

    array pop

--- js/array.js
+++ js/array.js
@@ -5,7 +5,13 @@ console.log(drinks.length)
 var drink = drinks[1]
 console.log(drink)
 
-// 値追加
+// 値（要素）追加
 drinks.push('炭酸水')
 console.log(drinks)
 console.log(drinks.length)
+
+// 値（要素）削除
+drinks.pop()
+console.log(drinks)
+console.log(drinks.length)
+
