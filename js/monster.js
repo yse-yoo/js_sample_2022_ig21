7893e32eb897ac787bac54d69144406a70acb512
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-10-26 11:47:53 +0900

    add question monster image

--- js/monster.js
+++ js/monster.js
@@ -23,3 +23,23 @@ a.target = "_blank"
 a.innerHTML = monsterElement1.innerHTML
 
 stage.appendChild(a)
+
+// 問題
+const createMonster = (monsterName, imageName) => { 
+    var monsterFrame = document.createElement('div')
+    var monsterElement = document.createElement('p')
+    var monsterImage = document.createElement('img')
+    // <p>xxxxx</p>
+    monsterElement.innerHTML = monsterName
+    // <img src="./images/xxxxx.jpg"/>
+    monsterImage.src = "./images/" + imageName + '.png'
+    // div に p と img を追加
+    monsterFrame.appendChild(monsterElement)
+    monsterFrame.appendChild(monsterImage)
+    // stage に div を追加
+    stage.appendChild(monsterFrame)
+}
+
+createMonster('モンスター1', 'monster_006')
+createMonster('モンスター2', 'monster_022')
+createMonster('モンスター3', 'monster_040')
