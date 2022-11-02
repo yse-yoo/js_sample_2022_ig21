6fe877779a8c8b311fd2d0314a03ee29595108a7
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-11-02 11:14:44 +0900

    for sum

--- js/array_multi.js
+++ js/array_multi.js
@@ -13,3 +13,14 @@ var users = [
     { id: 3, name: 'User3', email: 'user3@test.com', tel: '03-3111-2222'},
 ]
 console.log(users)
+
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+//1から1000まで足した合計値
+var sum = 0
+for (let i = 1; i <= 1000; i++) {
+   sum += i 
+}
+console.log(sum)
