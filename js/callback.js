8b600ade140afe3cda000eb107b84b36c9ecd5c6
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-11-16 12:09:57 +0900

    add filter

--- js/callback.js
+++ js/callback.js
@@ -18,3 +18,15 @@ new_scores = scores.filter(function(score) {
     return score >= 70
 })
 console.log(new_scores)
+
+// 合計値
+var sum = scores.reduce(function(prev, current) {
+    return prev + current 
+})
+console.log(sum)
+
+
+//70以上80未満のデータのみ作成
+new_scores = scores.filter((score) => score >= 70)
+                   .filter((score) => score < 80)
+console.log(new_scores)
