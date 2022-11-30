a2060b1146f9f92a3e2624ec463a3f97161e4384
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-11-30 10:01:35 +0900

    add foreach

--- js/selector.js
+++ js/selector.js
@@ -4,5 +4,10 @@ console.log(title)
 title = document.querySelector("#title")
 console.log(title)
 
-var cities = document.querySelector('.city')
+var cities = document.querySelectorAll('.city')
 console.log(cities)
+
+// 繰り返し表示
+cities.forEach(function(city) {
+    console.log(city.innerHTML)
+})
