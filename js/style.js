6c2b6948ea32eae1659c4e3de4025b1f8e3ab467
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-12-07 10:30:20 +0900

    add style js

---
+++ js/style.js
@@ -0,0 +1,13 @@
+
+const add = () => {
+    console.log('Add!!!')
+}
+const remove = () => {
+    console.log('remove!!!')
+}
+const toggle = () => {
+    console.log('toggle!!!')
+}
+const replace = () => {
+    console.log('replace!!!')
+}
