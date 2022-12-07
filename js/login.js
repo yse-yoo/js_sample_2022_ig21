b16b46a50ad12a64de54e3b5896efd7d7b67852b
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-12-07 10:06:40 +0900

    add login sample

--- js/login.js
+++ js/login.js
@@ -1,5 +1,25 @@
 
 function login() {
     console.log('Login!!!')
-    return false
+    var emailElements = document.getElementsByName('email')
+    var passwordElements = document.getElementsByName('password')
+
+    // console.log(emailElements[0])
+    // console.log(passwordElements[0])
+
+    var email = emailElements[0].value
+    var password = passwordElements[0].value
+
+    console.log(email, password)
+
+    var error_email = ""
+    var error_password = ""
+
+    if (!email) error_email = "Emailを入力してください"
+    if (!password) error_password = "パスワードを入力してください"
+
+    document.getElementById('message_email').innerHTML = error_email
+    document.getElementById('message_password').innerHTML = error_password
+
+    return !(error_email || error_password)
 }
