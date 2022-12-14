bf5bf1eb8ddbf41f0c3b41b8e7de6d1fcb17361f
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-12-14 10:36:40 +0900

    add slide animations

--- js/animation2.js
+++ js/animation2.js
@@ -6,19 +6,25 @@ $(() => {
 
     function slideToRight() {
         photo.animate(
-            { left: 300 }, 1000
+            { left: 300 }, 1000, 'swing', slideToDown
         )
     }
 
     function slideToLeft() {
-
+        photo.animate(
+            { left: 0 }, 1000, 'swing', slideToUp
+        )
     }
 
     function slideToUp() {
-
+        photo.animate(
+            { top: 0 }, 1000, 'swing', slideToRight
+        )
     }
 
     function slideToDown() {
-
+        photo.animate(
+            { top: 300 }, 1000, 'swing', slideToLeft
+        )
     }
 })
