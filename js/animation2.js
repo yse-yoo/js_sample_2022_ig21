d7b586130699f1073b9fc5063416e548346e5b66
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-12-14 10:54:39 +0900

    add animations

--- js/animation2.js
+++ js/animation2.js
@@ -2,29 +2,42 @@ var photo = $('#photo')
 
 $(() => {
 
-    slideToRight()
+    // slideToRight()
+
+    // click event を追加
+    $('#startBtn').on('click', slideToRight)
+    $('#stopBtn').on('click', stop)
+
+    $('#rightBtn').on('click', slideToRight)
+    $('#leftBtn').on('click', slideToLeft)
+    $('#downBtn').on('click', slideToDown)
+    $('#upBtn').on('click', slideToUp)
+
+    function stop() { 
+        photo.stop()
+    }
 
     function slideToRight() {
         photo.animate(
-            { left: 300 }, 1000, 'swing', slideToDown
+            { left: 300 }, 1000, 'swing'
         )
     }
 
     function slideToLeft() {
         photo.animate(
-            { left: 0 }, 1000, 'swing', slideToUp
+            { left: 0 }, 1000, 'swing'
         )
     }
 
     function slideToUp() {
         photo.animate(
-            { top: 0 }, 1000, 'swing', slideToRight
+            { top: 0 }, 1000, 'swing'
         )
     }
 
     function slideToDown() {
         photo.animate(
-            { top: 300 }, 1000, 'swing', slideToLeft
+            { top: 300 }, 1000, 'swing'
         )
     }
 })
