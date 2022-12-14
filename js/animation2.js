d477b29917b71ce8a47617098a15384f4bc49741
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-12-14 10:29:35 +0900

    add right

---
+++ js/animation2.js
@@ -0,0 +1,24 @@
+var photo = $('#photo')
+
+$(() => {
+
+    slideToRight()
+
+    function slideToRight() {
+        photo.animate(
+            { left: 300 }, 1000
+        )
+    }
+
+    function slideToLeft() {
+
+    }
+
+    function slideToUp() {
+
+    }
+
+    function slideToDown() {
+
+    }
+})
