954356a1823da649d72c945afc1f2bb7a505ffcd
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-12-14 11:37:02 +0900

    add ajax request

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,16 @@
 
 // id=getText のElementをクリックしたら
 $('#getText').on('click', () => {
-    console.log('ok')
+    const href = location.href
+    const index = href.lastIndexOf('/') + 1;
+    const baseUrl = href.substring(0, index);
+    const apiUrl = baseUrl + 'data/foods.json'
+
+    $.ajax({
+        type: "get",
+        url: apiUrl,
+        dataType: "text",
+    }).done((data) => {
+        console.log(data)
+    });
 })
