7ce9c6bf33881d9b850a179c10b2415de0a3b14a
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-12-14 09:55:07 +0900

    add fadeout

--- js/animation.js
+++ js/animation.js
@@ -6,7 +6,11 @@ photo.hide()
 
 function open() {
     // 3秒のフェードイン
-    photo.fadeIn(3000);
+    photo.fadeIn(3000, null, close);
+}
+
+function close() {
+    photo.fadeOut(3000, null, open);
 }
 
 // HTML読み込み終了後に処理
